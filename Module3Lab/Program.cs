53f62ccf088feddf38bcea6fa55cce6a01bf56f8
Andrew <andrewmgunn26@gmail.com>
2021-09-05 15:18:43 -0500

    ahve 3 methods done

--- Module3Lab/Program.cs
+++ Module3Lab/Program.cs
@@ -4,9 +4,43 @@ namespace Module3Lab
 {
     class Program
     {
+
+        static double BigDayPass(double days, double minutes)
+        {
+            double costDay = 10.00;
+            double costMin = 0;
+            return costDay * days;
+        }
+        
+        static double TouristPass(double days, double minutes)
+        {
+            double costday = 0;
+            double costMin = 0.20;
+            return minutes * costMin;
+        }
+        
+        static double AdventurerPass(double days, double minutes)
+        {
+            double costday = 3.0;
+            double costMin = 0.10;
+            return (costday * days) + (costMin * minutes);
+        }
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double days = 0;
+            double minutes = 0;
+            double adventure = 0;
+            
+            
+            Console.Write("How many days will you be travelling?>");
+            Double.TryParse(Console.ReadLine(), out days);
+            Console.Write("How many minutes per days will you use?>");
+            Double.TryParse(Console.ReadLine(), out minutes);
+
+            
+            
         }
     }
 }
